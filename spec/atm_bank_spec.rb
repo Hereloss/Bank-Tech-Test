e888f71a290f147b0d2ba6c147f30e2442059a4d
Chris Thomas <hereloss@googlemail.com>
2022-01-31 16:53:50 +0000

    Tests added for atm_bank

---
+++ spec/atm_bank_spec.rb
@@ -0,0 +1,44 @@
+require './atm_bank'
+
+describe Atm_Bank do
+
+  it 'The bank can return the current balance' do
+    expect(subject.my_balance).to eq(0)
+  end
+
+  context 'Deposits' do
+
+    it 'A customer can make a deposit to their account' do
+      subject.make_deposit(5)
+      expect(subject.my_balance).to eq(5)
+    end
+
+    it 'A customer can make multiple deposits to their account' do
+      subject.make_deposit(5)
+      subject.make_deposit(6)
+      expect(subject.my_balance).to eq(11)
+    end
+
+  end
+
+  context 'Withdrawals' do
+    it 'A customer can make a withdrawal' do
+      subject.make_deposit(5)
+      subject.make_withdrawal(5)
+      expect(subject.my_balance).to eq(0)
+    end
+
+    it 'A customer can make multiple withdrawals' do
+      subject.make_deposit(5)
+      subject.make_withdrawal(1)
+      subject.make_withdrawal(1)
+      expect(subject.my_balance).to eq(3)
+    end
+
+    it 'A customer cannot withdraw more money than they have in their account' do
+      expect(subject.valid_withdrawal(10)).to eq false
+    end
+
+  end
+ 
+end
---
+++ spec/atm_printer_spec.rb
@@ -0,0 +1,17 @@
+require './atm_bank'
+
+describe Atm_Bank do
+
+  context 'Account history General' do
+
+  end
+
+  context 'Deposits' do
+
+  end
+
+  context 'Withdrawals' do
+
+  end
+ 
+end
