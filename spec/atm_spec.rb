8e3f72608a4fef22bed001623f870f077c0a28c1
Chris Thomas <hereloss@googlemail.com>
2022-01-31 14:42:43 +0000

    Further tests added for withdrawal

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,29 +2,50 @@ require './atm.rb'
 
 describe Atm do
 
-  it 'A customer has a bank account balance' do
-    expect(subject.balance).not_to eq(nil)
+  context 'Balance' do
+
+    it 'A customer has a bank account balance' do
+      expect(subject.balance).not_to eq(nil)
+    end
+
+    it 'When a customer asks for their balance, it returns a number' do
+      expect(subject.balance).to eq(0)
+    end
   end
 
-  it 'When a customer asks for their balance, it returns a number' do
-    expect(subject.balance).to eq(0)
+  context 'Deposits' do
+    it 'A customer can make a deposit to their account' do
+      subject.deposit(5)
+      expect(subject.balance).to eq(5)
+    end
+
+    it 'A customer can make multiple deposits to their account' do
+      subject.deposit(5)
+      subject.deposit(6)
+      expect(subject.balance).to eq(11)
+    end
   end
 
-  it 'A customer can make a deposit to their account' do
-    subject.deposit(5)
-    expect(subject.balance).to eq(5)
-  end
+  context 'Withdrawals' do
+    it 'A customer can make a withdrawal' do
+      subject.deposit(5)
+      subject.withdraw(5)
+      expect(subject.balance).to eq(0)
+    end
 
-  it 'A customer can make multiple deposits to their account' do
-    subject.deposit(5)
-    subject.deposit(6)
-    expect(subject.balance).to eq(11)
-  end
+    it 'A customer can make multiple withdrawals' do
+      subject.deposit(5)
+      subject.withdraw(1)
+      subject.withdraw(1)
+      expect(subject.balance).to eq(3)
+    end
 
-  it 'A customer can make a withdrawal' do
-    subject.deposit(5)
-    subject.withdraw(5)
-    expect(subject.balance).to eq(0)
+    it 'A customer cannot withdraw more money than they have in their account' do
+      subject.deposit(5)
+      expect(subject.withdraw(10)).to eq 'Error - Not enough money!'
+      expect { "Error - Not enough money!" }.to output.to_stdout
+    end
+
   end
 
 end
