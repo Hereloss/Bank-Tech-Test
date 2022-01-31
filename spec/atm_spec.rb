585eea63a1cc282ac5794d39016de5eecdaa87e6
Chris Thomas <hereloss@googlemail.com>
2022-01-31 17:29:57 +0000

    Doubles tests strengthened

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -10,12 +10,12 @@ describe Atm do
   end
   context 'Balance' do
     it 'A customer has a bank account balance' do
-      allow(@bank_double).to receive(:my_balance).and_return(0)
+      expect(@bank_double).to receive(:my_balance).and_return(0).twice
       expect(@atm.check_balance).not_to eq(nil)
     end
 
     it 'When a customer asks for their balance, it returns a number' do
-      allow(@bank_double).to receive(:my_balance).and_return(0)
+      expect(@bank_double).to receive(:my_balance).and_return(0).twice
       expect(@atm.check_balance).to eq(0)
     end
   end
@@ -23,18 +23,18 @@ describe Atm do
   context 'Deposits' do
     it 'A customer can make a deposit to their account' do
       allow(@bank_double).to receive(:my_balance).and_return(5)
-      allow(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
-      allow(@printer_double).to receive(:update_account_history).and_return(['5, 31-01-2022'])
+      expect(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
+      expect(@printer_double).to receive(:update_account_history).and_return(['5, 31-01-2022'])
       @atm.deposit(5)
       expect(@atm.check_balance).to eq(5)
     end
 
     it 'A customer can make multiple deposits to their account' do
       allow(@bank_double).to receive(:my_balance).and_return(11)
-      allow(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
+      expect(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
       allow(@printer_double).to receive(:update_account_history).and_return(['5, 31-01-2022'])
-      subject.deposit(5)
-      allow(@bank_double).to receive(:make_deposit).and_return("6, 31-01-2022")
+      @atm.deposit(5)
+      expect(@bank_double).to receive(:make_deposit).and_return("6, 31-01-2022")
       allow(@printer_double).to receive(:update_account_history).and_return(['6, 31-01-2022'])
       @atm.deposit(6)
       expect(@atm.check_balance).to eq(11)
@@ -42,8 +42,8 @@ describe Atm do
 
     it 'Each deposit has a timestamp on it stating the date it was made on' do
       allow(@bank_double).to receive(:my_balance).and_return(11)
-      allow(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
-      allow(@printer_double).to receive(:update_account_history).and_return(["date || credit || debit || balance","#{Date.today.strftime('%d-%m-%Y')} || 5 || || 5]"])
+      expect(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
+      expect(@printer_double).to receive(:update_account_history).and_return(["date || credit || debit || balance","#{Date.today.strftime('%d-%m-%Y')} || 5 || || 5]"])
       expect(@atm.deposit(5)[1]).to include(Date.today.strftime('%d-%m-%Y').to_s)
     end
 
@@ -60,9 +60,14 @@ describe Atm do
 
   context 'Withdrawals' do
     it 'A customer can make a withdrawal' do
-      subject.deposit(5)
-      subject.withdraw(5)
-      expect(subject.check_balance).to eq(0)
+      allow(@bank_double).to receive(:my_balance).and_return(0)
+      expect(@bank_double).to receive(:make_deposit).and_return("5, 31-01-2022")
+      expect(@bank_double).to receive(:make_withdrawal).and_return("5, 31-01-2022")
+      expect(@bank_double).to receive(:valid_withdrawal).and_return(true)
+      allow(@printer_double).to receive(:update_account_history).and_return(["date || credit || debit || balance","#{Date.today.strftime('%d-%m-%Y')} || 5 || || 5]"])
+      @atm.deposit(5)
+      @atm.withdraw(5)
+      expect(@atm.check_balance).to eq(0)
     end
 
     it 'A customer can make multiple withdrawals' do
